5f9aacd8b7ee2326a190f0b8cc0c85ea5ef20753
王卫 <2270897140@qq.com>
2021-07-04 22:04:16 +0800

    [微服务项目初始化提交]

---
+++ cloud-provider-payment8001/src/main/java/com/wangw/springcloud/service/PaymentService.java
@@ -0,0 +1,13 @@
+package com.wangw.springcloud.service;
+
+
+import com.wangw.springcloud.entities.Payment;
+import org.apache.ibatis.annotations.Param;
+
+public interface PaymentService {
+
+    public int create(Payment payment);
+
+    public Payment getPaymentById(@Param("id") Long id);
+
+}
