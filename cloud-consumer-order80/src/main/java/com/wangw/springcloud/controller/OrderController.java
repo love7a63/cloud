89a5d9336134f658bb14c151e12d7123f73bdf9b
王卫 <2270897140@qq.com>
2021-07-05 22:08:26 +0800

    构建常用工具类项目

--- cloud-consumer-order80/src/main/java/com/wangw/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/wangw/springcloud/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.wangw.springcloud.controller;
 
 import com.wangw.springcloud.entities.CommonResult;
 import com.wangw.springcloud.entities.Payment;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
--- cloud-provider-payment8001/src/main/java/com/wangw/springcloud/controller/Payment8001Controller.java
+++ cloud-provider-payment8001/src/main/java/com/wangw/springcloud/controller/Payment8001Controller.java
@@ -6,6 +6,7 @@ package com.wangw.springcloud.controller;
 import com.wangw.springcloud.entities.CommonResult;
 import com.wangw.springcloud.entities.Payment;
 import com.wangw.springcloud.service.PaymentService;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
