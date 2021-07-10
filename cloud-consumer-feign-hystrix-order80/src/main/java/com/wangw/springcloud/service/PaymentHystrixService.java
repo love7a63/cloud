665190ae423837bdea70929d1eabd06cd6a7fb00
王卫 <2270897140@qq.com>
2021-07-10 14:40:17 +0800

    Hystirx 服务降级 提交

--- cloud-consumer-feign-hystrix-order80/src/main/java/com/wangw/springcloud/service/PaymentHystrixService.java
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/wangw/springcloud/service/PaymentHystrixService.java
@@ -1,12 +1,14 @@
 package com.wangw.springcloud.service;
 
+import com.wangw.springcloud.service.impl.PaymentFallbackService;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Component
-@FeignClient(value = "CLOUD-PROVIDER-HYSTRIX-PAYMENT")
+@FeignClient(value = "CLOUD-PROVIDER-HYSTRIX-PAYMENT",
+            fallback = PaymentFallbackService.class)//指定PaymentFallbackService类
 public interface PaymentHystrixService {
     @GetMapping("/payment/hystrix/ok/{id}")
     public String paymentInfo_OK(@PathVariable("id") Integer id);
