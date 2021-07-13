f2b765d6a1911a536fc6f5a47db950c4db3259d6
wangwei <2270897140@qq.com>
2021-07-13 19:30:22 +0800

    seata 订单模块 提交

---
+++ seata-order-service2001/src/main/java/com/wangw/cloudalibaba/service/AccountService.java
@@ -0,0 +1,21 @@
+package com.wangw.cloudalibaba.service;
+
+import com.wangw.cloudalibaba.entities.CommonResult;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.math.BigDecimal;
+
+/**
+ * @description:
+ * @author: wangwei
+ * @date: 2021/7/13 19:14
+ */
+@FeignClient(value = "seata-account-service")
+public interface AccountService {
+
+    @PostMapping(value = "/account/decrease")
+    CommonResult decrease(@RequestParam("userId") Long userId, @RequestParam("money") BigDecimal money);
+
+}
